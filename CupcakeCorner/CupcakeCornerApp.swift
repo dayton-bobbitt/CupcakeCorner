05235d765b06cd5f4c235a1b492ba44790d2c63d
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2021-11-13 15:50:04 -0600

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Dayton Bobbitt on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
