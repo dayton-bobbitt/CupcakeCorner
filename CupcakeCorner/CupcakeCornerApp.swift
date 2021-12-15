d08714e98a99756fcccbc3ac2137094a8a5681d7
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2021-12-14 20:14:34 -0600

    Customize color of UIKit views

--- CupcakeCorner/CupcakeCornerApp.swift
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -12,6 +12,12 @@ struct CupcakeCornerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    let tertiaryButtonTextColor = UIColor(named: "ColorTextButtonTertiary")!
+                    
+                    UINavigationBar.appearance().tintColor = tertiaryButtonTextColor
+                    UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).tintColor = tertiaryButtonTextColor
+                }
         }
     }
 }
